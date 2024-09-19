4edfc8a3e0814e56d4e7a37925996bc12828517a
guille barrio <gbarrio95@gmail.com>
2024-09-19 11:03:29 +0200

    3

--- modelo_proy.py
+++ modelo_proy.py
@@ -82,7 +82,7 @@ reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr
 early_stop = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 
 # Entrenamiento del modelo
-model1.fit(X_train, y_train, epochs=80, validation_data=(X_validation, y_validation), callbacks=[reduce_lr, early_stop])
+history1 = model1.fit(X_train, y_train, epochs=80, validation_data=(X_validation, y_validation), callbacks=[reduce_lr, early_stop])
 
 # Guarda el modelo en un archivo
 # model1.save('C:\\Users\\guill\\OneDrive\\Desktop\\main\\model_nvda')
@@ -90,3 +90,49 @@ model1.fit(X_train, y_train, epochs=80, validation_data=(X_validation, y_validat
 # # Cargar el modelo
 # loaded_model = load_model('C:\\Users\\guill\\OneDrive\\Desktop\\main\\model_nvda')
 # print("Modelo cargado exitosamente.")
+
+# Graficar los datos filtrados
+plt.plot(data.index, data['Close'])
+plt.title(f'{symbol} Stock Prices (2020 - 2024)')
+plt.xlabel('Date')
+plt.ylabel('Close Price')
+plt.show()
+
+
+# Filtrar las fechas para cada conjunto
+train_data_len = train_size
+val_data_len = train_size + validation_size
+test_data_len = train_size + validation_size + len(X_test)
+
+data_train = data.iloc[:train_data_len]
+data_val = data.iloc[train_data_len:val_data_len]
+data_test = data.iloc[val_data_len:test_data_len]
+
+# Graficar las tres partes(Train, Test, Validation)
+plt.figure(figsize=(10, 6))
+plt.plot(data_train.index, data_train['Close'], label='Train', color='blue')
+plt.plot(data_val.index, data_val['Close'], label='Validation', color='orange')
+plt.plot(data_test.index, data_test['Close'], label='Test', color='green')
+
+# Agregar títulos y leyendas
+plt.title(f'{symbol} Stock Prices - Train, Validation y Test')
+plt.xlabel('Date')
+plt.ylabel('Close Price')
+plt.legend()
+
+# Mostrar el gráfico
+plt.show()
+
+# Visualización de la función Loss
+plt.plot(history1.history['loss'],label='loss')
+plt.plot(history1.history['val_loss'],label='val_loss')
+
+# Agregar títulos y leyendas
+plt.title(f'{symbol} Loss Function')
+plt.xlabel('Epochs')
+plt.legend()
+plt.show()
+
+# Evaluación en el conjunto de prueba
+test = model1.evaluate(X_test, y_test)
+print(f'Test loss: {test}')
